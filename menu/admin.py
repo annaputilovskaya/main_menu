c34a0267dc5d7e12009f9c5d3e09195227fc8449
putilovskaya <putilovskayaa@mail.ru>
2024-10-07 00:16:44 +0300

    create MenuAdmin and MenuItemAdmin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import Menu, MenuItem
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name"]
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ["pk", "menu", "name", "named_url", "parent"]
+    list_display_links = ["name"]
+    list_filter = ["menu"]
